8a44863cd0a46285387c36dd35a3a49004a19ee9
TaylorSchmautz <TaylorSchmautz@hotmail.com>
2017-03-04 14:05:51 -0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -23,7 +23,7 @@ def start():
     # TODO: Do things with data
 
     return {
-        'color': '#FF00FF',
+        'color': '#FFFFFF',
         'taunt': '{} ({}x{})'.format(game_id, board_width, board_height),
         'head_url': head_url,
         'name': 'King snake AKA Owen'
