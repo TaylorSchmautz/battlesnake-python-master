21e7323ffe09f8e4296b93f80a50e2c41275baca
TaylorSchmautz <TaylorSchmautz@hotmail.com>
2017-03-04 14:49:57 -0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -33,9 +33,9 @@ def start():
 @bottle.post('/move')
 def move():
     data = bottle.request.json
-    #food = bottle.request.json(u'food')
-    #snake = bottle.request.json(u'snakes')
-    #me = data[u'you']
+    food = bottle.request.json(u'food')
+    snake = bottle.request.json(u'snakes')
+    me = data[u'you']
 
     # TODO: Do things with data
     directions = ['up', 'down', 'left', 'right']
